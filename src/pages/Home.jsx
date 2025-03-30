928d236758cf48f3f769cacabb1f9e277753aea4
Sahil Pathak <sahilpathak2005@gmail.com>
2025-03-30 19:35:33 +0530

    Add missing files to version control

---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Card from '../components/Card'
+import MarqueeAnimation from '../components/MarqueeAnimation'
+import Myprojects from '../projects/Myprojects'
+import Message from '../components/Message'
+
+const Home = () => {
+  return (
+    <div className="bg-[#111111] min-h-screen">
+      <Card />
+      <MarqueeAnimation />
+      <div className="text-white text-2xl font-bold text-center py-4">
+        My Work
+      </div>
+      <div className="flex flex-wrap justify-center gap-4 p-4">
+        <Myprojects /> 
+      </div> 
+      <div className='flex justify-center space-x-4'>
+        <Message />
+      </div>
+    </div>
+  )
+}
+
+export default Home
